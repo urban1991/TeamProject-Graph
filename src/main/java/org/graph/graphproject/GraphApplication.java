15c79e18bd7457544d17e1ca0d82e68712b41d5d
piotr.urbaniak <piotr.urbaniak.extern@sport1.de>
2025-12-03 20:01:39 +0100

    feat: implement AStar algorithm

--- src/main/java/org/graph/graphproject/GraphApplication.java
+++ src/main/java/org/graph/graphproject/GraphApplication.java
@@ -10,7 +10,7 @@ public class GraphApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GraphApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 800);
         stage.setTitle("Graph Application!");
         stage.setScene(scene);
         stage.show();
